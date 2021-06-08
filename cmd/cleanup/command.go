fa86528ab278971db106bfe5f3e8a99e0307c18f
chengshiwen <chengshiwen0103@gmail.com>
2021-06-08 20:33:26 +0800

    remove ants and optimize cleanup command

--- cmd/cleanup/command.go
+++ cmd/cleanup/command.go
@@ -9,32 +9,31 @@ import (
 
 	_ "github.com/influxdata/influxdb1-client" // this is important because of the bug in go mod
 	client "github.com/influxdata/influxdb1-client/v2"
-	"github.com/panjf2000/ants/v2"
 	"github.com/spf13/cobra"
 )
 
-type flags struct {
-	Host     string
-	Port     int
-	Database string
-	Username string
-	Password string
-	Ssl      bool
-	Regexp   string
-	MaxLimit int
-	ShowNum  int
-	DropNum  int
-	Worker   int
-	Progress int
-	Cleanup  bool
+type flagpole struct {
+	host     string
+	port     int
+	database string
+	username string
+	password string
+	ssl      bool
+	regexp   string
+	maxLimit int
+	showNum  int
+	dropNum  int
+	worker   int
+	progress int
+	cleanup  bool
 }
 
 func NewCommand() *cobra.Command {
-	flags := &flags{}
+	flags := &flagpole{}
 	cmd := &cobra.Command{
 		Args:          cobra.NoArgs,
 		Use:           "cleanup",
-		Short:         "Cleanup unused measurements",
+		Short:         "cleanup measurements",
 		SilenceUsage:  true,
 		SilenceErrors: true,
 		RunE: func(c *cobra.Command, args []string) error {
@@ -43,68 +42,68 @@ func NewCommand() *cobra.Command {
 		},
 	}
 	cmd.Flags().SortFlags = false
-	cmd.Flags().StringVarP(&flags.Host, "host", "H", "127.0.0.1", "host to connect to")
-	cmd.Flags().IntVarP(&flags.Port, "port", "P", 8086, "port to connect to")
-	cmd.Flags().StringVarP(&flags.Database, "database", "d", "", "database to connect to the server (required)")
-	cmd.Flags().StringVarP(&flags.Username, "username", "u", "", "username to connect to the server")
-	cmd.Flags().StringVarP(&flags.Password, "password", "p", "", "password to connect to the server")
-	cmd.Flags().BoolVarP(&flags.Ssl, "ssl", "s", false, "use https for requests")
-	cmd.Flags().StringVarP(&flags.Regexp, "regexp", "r", "", "regular expression of measurements to clean (default \"\", all)")
-	cmd.Flags().IntVarP(&flags.MaxLimit, "max-limit", "m", 0, "max limit to show measurements (default 0, no limit)")
-	cmd.Flags().IntVarP(&flags.ShowNum, "show-num", "S", 5, "measurement number to show when show measurements")
-	cmd.Flags().IntVarP(&flags.DropNum, "drop-num", "D", 10, "measurement number to drop per worker")
-	cmd.Flags().IntVarP(&flags.Worker, "worker", "w", 10, "number of concurrent workers to cleanup")
-	cmd.Flags().IntVarP(&flags.Progress, "progress", "n", 100, "print progress after every <n> measurements cleanup")
-	cmd.Flags().BoolVarP(&flags.Cleanup, "cleanup", "C", false, "confirm cleanup the measurements (be cautious before doing it)")
+	cmd.Flags().StringVarP(&flags.host, "host", "H", "127.0.0.1", "host to connect to")
+	cmd.Flags().IntVarP(&flags.port, "port", "P", 8086, "port to connect to")
+	cmd.Flags().StringVarP(&flags.database, "database", "d", "", "database to connect to the server (required)")
+	cmd.Flags().StringVarP(&flags.username, "username", "u", "", "username to connect to the server")
+	cmd.Flags().StringVarP(&flags.password, "password", "p", "", "password to connect to the server")
+	cmd.Flags().BoolVarP(&flags.ssl, "ssl", "s", false, "use https for requests")
+	cmd.Flags().StringVarP(&flags.regexp, "regexp", "r", "", "regular expression of measurements to clean (default \"\", all)")
+	cmd.Flags().IntVarP(&flags.maxLimit, "max-limit", "m", 0, "max limit to show measurements (default 0, no limit)")
+	cmd.Flags().IntVarP(&flags.showNum, "show-num", "S", 5, "measurement number to show when show measurements")
+	cmd.Flags().IntVarP(&flags.dropNum, "drop-num", "D", 10, "measurement number to drop per worker")
+	cmd.Flags().IntVarP(&flags.worker, "worker", "w", 10, "number of concurrent workers to cleanup")
+	cmd.Flags().IntVarP(&flags.progress, "progress", "n", 100, "print progress after every <n> measurements cleanup")
+	cmd.Flags().BoolVarP(&flags.cleanup, "cleanup", "C", false, "confirm cleanup the measurements (be cautious before doing it)")
 	cmd.MarkFlagRequired("database")
 	return cmd
 }
 
-func processFlags(flags *flags) {
-	if flags.MaxLimit < 0 {
-		log.Fatal("invalid max-limit")
+func processFlags(flags *flagpole) {
+	if flags.maxLimit < 0 {
+		log.Fatal("max-limit is invalid")
 	}
-	if flags.ShowNum <= 0 {
-		log.Fatal("invalid show-num")
+	if flags.showNum <= 0 {
+		log.Fatal("show-num is invalid")
 	}
-	if flags.DropNum <= 0 {
-		log.Fatal("invalid drop-num")
+	if flags.dropNum <= 0 {
+		log.Fatal("drop-num is invalid")
 	}
-	if flags.Worker <= 0 {
-		log.Fatal("invalid worker")
+	if flags.worker <= 0 {
+		log.Fatal("worker is invalid")
 	}
-	if flags.Progress <= 0 {
-		log.Fatal("invalid progress")
+	if flags.progress <= 0 {
+		log.Fatal("progress is invalid")
 	}
 }
 
-func runE(flags *flags) (err error) {
-	addr := fmt.Sprintf("http://%s:%d", flags.Host, flags.Port)
-	if flags.Ssl {
-		addr = fmt.Sprintf("https://%s:%d", flags.Host, flags.Port)
+func runE(flags *flagpole) (err error) {
+	addr := fmt.Sprintf("http://%s:%d", flags.host, flags.port)
+	if flags.ssl {
+		addr = fmt.Sprintf("https://%s:%d", flags.host, flags.port)
 	}
 	c, err := client.NewHTTPClient(client.HTTPConfig{
 		Addr:               addr,
-		Username:           flags.Username,
-		Password:           flags.Password,
-		InsecureSkipVerify: flags.Ssl,
+		Username:           flags.username,
+		Password:           flags.password,
+		InsecureSkipVerify: flags.ssl,
 	})
 	if err != nil {
-		log.Println("creating influxdb client error: ", err.Error())
+		log.Printf("creating influxdb client error: %v", err)
 		return
 	}
 	defer c.Close()
 
 	var measurements []string
 	query := "SHOW MEASUREMENTS"
-	if flags.Regexp != "" {
-		query = fmt.Sprintf("%s WITH MEASUREMENT =~ /%s/", query, flags.Regexp)
+	if flags.regexp != "" {
+		query = fmt.Sprintf("%s WITH MEASUREMENT =~ /%s/", query, flags.regexp)
 	}
-	if flags.MaxLimit > 0 {
-		query = fmt.Sprintf("%s LIMIT %d", query, flags.MaxLimit)
+	if flags.maxLimit > 0 {
+		query = fmt.Sprintf("%s LIMIT %d", query, flags.maxLimit)
 	}
 	log.Printf("query: %s", query)
-	q := client.NewQuery(query, flags.Database, "")
+	q := client.NewQuery(query, flags.database, "")
 	if response, err := c.Query(q); err == nil && response.Error() == nil {
 		results := response.Results
 		if len(results) > 0 {
@@ -118,12 +117,12 @@ func runE(flags *flags) (err error) {
 			}
 		}
 	}
-	if len(measurements) > flags.ShowNum {
-		log.Printf("measurements: %v ... (total %d)", strings.Join(measurements[:flags.ShowNum], " "), len(measurements))
+	if len(measurements) > flags.showNum {
+		log.Printf("measurements: %v ... (total %d)", strings.Join(measurements[:flags.showNum], " "), len(measurements))
 	} else if len(measurements) > 0 {
 		log.Printf("measurements: %v (total %d)", strings.Join(measurements, " "), len(measurements))
 	} else {
-		log.Println("measurements: empty (total 0)")
+		log.Print("measurements: empty (total 0)")
 		return
 	}
 
@@ -131,18 +130,17 @@ func runE(flags *flags) (err error) {
 	return
 }
 
-func cleanup(flags *flags, c client.Client, measurements []string) {
-	if flags.Cleanup {
-		log.Println("cleanup measurements ...")
-		pool, _ := ants.NewPool(flags.Worker)
-		defer pool.Release()
+func cleanup(flags *flagpole, c client.Client, measurements []string) {
+	if flags.cleanup {
+		log.Print("cleanup measurements ...")
+		limit := make(chan struct{}, flags.worker)
 		wg := &sync.WaitGroup{}
 		var done int64
-		cycle := (len(measurements)-1)/flags.DropNum + 1
+		cycle := (len(measurements)-1)/flags.dropNum + 1
 		for i := 0; i < cycle; i++ {
-			queries := make([]string, 0, flags.DropNum)
-			start := i * flags.DropNum
-			end := (i + 1) * flags.DropNum
+			queries := make([]string, 0, flags.dropNum)
+			start := i * flags.dropNum
+			end := (i + 1) * flags.dropNum
 			if end > len(measurements) {
 				end = len(measurements)
 			}
@@ -152,25 +150,34 @@ func cleanup(flags *flags, c client.Client, measurements []string) {
 			}
 			query := strings.Join(queries, "; ")
 			wg.Add(1)
-			pool.Submit(func() {
-				defer wg.Done()
-				q := client.NewQuery(query, flags.Database, "")
+			go func() {
+				if flags.worker > 0 {
+					limit <- struct{}{}
+				}
+				defer func() {
+					wg.Done()
+					if flags.worker > 0 {
+						<-limit
+					}
+				}()
+
+				q := client.NewQuery(query, flags.database, "")
 				if response, err := c.Query(q); err == nil && response.Error() == nil {
 					atomic.AddInt64(&done, int64(len(response.Results)))
-					if atomic.LoadInt64(&done)%int64(flags.Progress) == 0 {
+					if atomic.LoadInt64(&done)%int64(flags.progress) == 0 {
 						log.Printf("%d/%d cleanup done", done, len(measurements))
 					}
 				} else if err != nil {
-					log.Printf("cleanup error: %s", err)
+					log.Printf("cleanup error: %v", err)
 				} else {
-					log.Printf("cleanup response error: %s", response.Error())
+					log.Printf("cleanup response error: %v", response.Error())
 				}
-			})
+			}()
 		}
 		wg.Wait()
-		if done%int64(flags.Progress) != 0 {
+		if done%int64(flags.progress) != 0 {
 			log.Printf("%d/%d cleanup done", done, len(measurements))
 		}
-		log.Println("cleanup measurements done")
+		log.Print("cleanup measurements done")
 	}
 }
